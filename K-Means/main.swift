8bef57b48845be7863c2bc9ce7dba7eb2f56887b
Justin Purnell <jpurnell7996@gmail.com>
2016-09-26 15:59:23 -0400

    Initial Commit

---
+++ K-Means/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  K-Means
+//
+//  Created by Justin Purnell on 9/26/16.
+//  Copyright © 2016 Justin Purnell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
